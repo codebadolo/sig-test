8143efc29d8236bd6bc597fe7b5f389462b875c4
codebadolo <codewithbadolo@gmail.com>
2024-04-08 11:19:03 +0000

    push     after adding the code  for the student management

--- frontendsig/src/components/shared/Sidebar.jsx
+++ frontendsig/src/components/shared/Sidebar.jsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames'
 import React from 'react'
-import { FcBullish } from 'react-icons/fc'
 import { HiOutlineLogout } from 'react-icons/hi'
 import { Link, useLocation } from 'react-router-dom'
 import { DASHBOARD_SIDEBAR_BOTTOM_LINKS, DASHBOARD_SIDEBAR_LINKS } from '../../lib/constants'
@@ -9,11 +8,14 @@ const linkClass =
 	'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base'
 
 export default function Sidebar() {
+ 
+	
+
 	return (
 		<div className="bg-neutral-900 w-60 p-3 flex flex-col">
 			<div className="flex items-center gap-2 px-1 py-3">
-				<FcBullish fontSize={24} />
-				<span className="text-neutral-200 text-lg">OpenShop</span>
+				
+				<span className="text-neutral-200 text-lg">Nassara  SIG </span>
 			</div>
 			<div className="py-8 flex flex-1 flex-col gap-0.5">
 				{DASHBOARD_SIDEBAR_LINKS.map((link) => (
@@ -37,7 +39,7 @@ export default function Sidebar() {
 
 function SidebarLink({ link }) {
 	const { pathname } = useLocation()
-
+	
 	return (
 		<Link
 			to={link.path}
